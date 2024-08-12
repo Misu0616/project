4d853c2cce35758bc04e68f47c559cd6b206456c
Misu0616 <cyniaa@naver.com>
2024-08-12 19:53:42 +0900

    0812Commit

---
+++ app/src/main/java/com/jica/project/addActivity.java
@@ -0,0 +1,2 @@
+package com.jica.project;public class addActivity {
+}
---
+++ app/src/main/java/com/jica/project/addProtectThing.java
@@ -0,0 +1,14 @@
+package com.jica.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addProtectThing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_protect_thing);
+    }
+}
